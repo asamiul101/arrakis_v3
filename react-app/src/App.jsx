98deb7f0f7ec16c0048561c001660af79f3a013c
Eric Lynch <ericdonaldlynch@gmail.com>
2023-08-04 19:53:11 +0100

    Added Router and Nav bar

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import Login from "./components/Login";
+import Home from "./components/Home";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
-  return <Login />;
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/home">Home</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="#adduser">Add User</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
--- react-app/src/components/Login.jsx
+++ react-app/src/components/Login.jsx
@@ -1,65 +1,85 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form'
-import Row from 'react-bootstrap/Row'
-import Button from 'react-bootstrap/Button'
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { getAllUsers } from '../services/UserServices';
-
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getAllUsers } from "../services/UserServices";
+import { getSpecificUsers } from "../services/UserServices";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
-        getUsersFromAPI();}, 
-        []
-);
-const getUsersFromAPI = ()=>{
+  useEffect(() => {
+    getUsersFromAPI();
+  }, []);
+
+  useEffect(() => {
+    getSpecificUsersFromAPI(username);
+  }, []);
+
+  const getSpecificUsersFromAPI = (username) => {};
+
+  const getUsersFromAPI = () => {
     getAllUsers()
-  .then(res => {
-      setUsers(res.data);
-  })
-  .catch(err => {
-      setUsers([]);
-      console.log(err);
-  })
-}
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setUsers([]);
+        console.log(err);
+      });
+  };
 
-    const handleUsernameChange = (event)=>{
-        setUsername(event.target.value);
-    }
-    const handlePasswordChange = (event)=>{
-        setPassword(event.target.value);
-    }
-    const handleSubmit =(event)=>{
-        event.preventDefault();
-        let user = {};
-        user.username = username;
-        user.password = password;
-        console.log(user);
-    }
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let user = {};
+    user.username = username;
+    user.password = password;
+    console.log(user);
+
+    return getSpecificUsers(username)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setUsers([]);
+        console.log(err);
+      });
+  };
   return (
-<Row className='LoginForm'>
-    <Form>
-      <Form.Group className="mb-3" controlId="username">
-         <Form.Label>Username: </Form.Label>
-            <Form.Control type="text" placeholder="Username" 
-            onChange={handleUsernameChange}/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="password">
-            <Form.Label>Password: </Form.Label>
-            <Form.Control type="text" placeholder="Password" 
-            onChange={handlePasswordChange}/>
-      </Form.Group>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-            Submit
-            </Button>
+    <Row className="LoginForm">
+      <Form>
+        <Form.Group className="mb-3" controlId="username">
+          <Form.Label>Username: </Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            onChange={handleUsernameChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password: </Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Password"
+            onChange={handlePasswordChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
       </Form>
-      {users}
-</Row>
-  )
-}
+    </Row>
+  );
+};
 
-export default Login
+export default Login;
